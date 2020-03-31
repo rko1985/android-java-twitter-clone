d984368539fed9563ef73c5cf832d18eea1b87d0
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-03-31 13:45:11 -1000

    login/signup page created and fixed gradle files

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -12,7 +12,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.Parse;
@@ -21,17 +23,49 @@ import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 
 public class MainActivity extends AppCompatActivity {
 
+  public void signupLogin(View view){
+      final EditText usernameEditText = findViewById(R.id.usernameEditText);
+      final EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+      ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+        @Override
+        public void done(ParseUser user, ParseException e) {
+          if(e == null){
+            //Login successful
+            Log.i("Login", "Success!");
+          } else {
+            //Create new account
+            ParseUser newUser = new ParseUser();
+            newUser.setUsername(usernameEditText.getText().toString());
+            newUser.setPassword(passwordEditText.getText().toString());
+
+            newUser.signUpInBackground(new SignUpCallback() {
+              @Override
+              public void done(ParseException e) {
+                if(e == null){
+                  Log.i("Signup", "Success!");
+                } else {
+                  Toast.makeText(MainActivity.this, e.getMessage().substring(e.getMessage().indexOf(" ")), Toast.LENGTH_SHORT).show();
+                }
+              }
+            });
+          }
+        }
+      });
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    
+    setTitle("Twitter: Login");
+
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
 
